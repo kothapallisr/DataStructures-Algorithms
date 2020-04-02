82a09252e025d28382ab6ca3c3752608da1a3410
Sridhar Kothapalli <sridr8@gmail.com>
2020-04-02 23:12:16 +0530

    Strings

---
+++ strings.py
@@ -0,0 +1,22 @@
+# Strings
+
+name = 'Brad'
+age = 37
+
+# concatenate
+print('Hello, my name is ' + name + ' and I am ' + str(age))
+
+# String formatting
+
+# Arguments by position
+print('My name is {name} and I am {age}'.format(name=name, age=age) )
+
+# F-Strings (3.6+)
+print(f'Hello, my name is {name} and I am {age}')
+
+# String methods
+s = 'Hello world'
+
+
+
+
