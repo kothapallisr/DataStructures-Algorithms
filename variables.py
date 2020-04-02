1c9d4797bc46fcde77468beb38bd470350e73c1b
Sridhar Kothapalli <sridr8@gmail.com>
2020-04-02 23:03:53 +0530

    variables

---
+++ variables.py
@@ -0,0 +1,27 @@
+print('Sri Sainathuni')
+# variables
+'''
+This is a multiline comment
+'''
+"""
+This is also a multiline comment
+"""
+x = 1 #i nt
+y = 2.5  # float
+name = 'Sri' # str
+is_cool = True # bool
+
+# Multiple assignment
+x,y,name,is_cool = (1,2.5,'Sri',True)
+
+print('Hello')
+a = x + y
+# casting
+x = str(x)
+y = int(y)
+z = float(y)
+print(type(x))
+print(type(z), z)
+
+
+
