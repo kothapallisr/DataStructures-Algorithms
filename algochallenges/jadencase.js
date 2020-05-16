36c7bfc3cdc444fddfefb44a92c939541214e7ca
Sridhar Kothapalli <sridr8@gmail.com>
2020-05-16 19:32:54 +0530

    Jaden case

---
+++ algochallenges/jadencase.js
@@ -0,0 +1,13 @@
+function capitalize(word) {
+  return word[0].toUpperCase() + word.slice(1).toLowerCase()
+}
+function toJadenCase(str) {
+  let words = str.split(" ");
+  let result = [];
+  for(let word of words){
+    result.push(capitalize(word));
+  }
+  return result.join(" ");
+}
+
+console.log(toJadenCase("How can mirrors be real if our eyes aren't real"));
