8d0c3b77fe0dd0d8f68106201a48c5943ccb709c
Sridhar Kothapalli <sridr8@gmail.com>
2020-05-19 23:18:06 +0530

    mumbling

--- algochallenges/mumbling.js
+++ algochallenges/mumbling.js
@@ -1,3 +1,21 @@
-function funkyCapitalize() {
-  
+function funkyCapitalize(letter, num) {
+  let result = "";
+  for(let i=0; i<num; i++) {
+    result += i === 0 ? letter.toUpperCase() : letter.toLowerCase();
+  }
+  return result;
 }
+
+function accum(str) {
+  let result = [];
+  let letters = str.split("");
+  let counter = 1;
+  for (letter of letters) {
+    result.push(funkyCapitalize(letter, counter));
+    counter++;
+  }
+  return result.join("-");
+}
+
+console.log(accum("Saibaba"));
+console.log(accum("Sarath Babuji"));
